8b47c4eed583cfb7a30948f30d5e9d4ac7cf96f5
alchemistmatt <matthew.monroe@pnnl.gov>
2020-01-21 09:48:45 -0800

    Update versions

--- docs/Convert_psi-ms.bat
+++ docs/Convert_psi-ms.bat
@@ -1,10 +1,14 @@
-..\bin\OBODataConverter.exe psi-ms_4.1.25.obo
-..\bin\OBODataConverter.exe psi-ms_4.1.25.obo  psi-ms_4.1.25_NoAncestors.txt /NoP
-..\bin\OBODataConverter.exe psi-ms_4.1.25.obo  psi-ms_4.1.25_WithParents.txt /NoG
-..\bin\OBODataConverter.exe psi-ms_4.1.25.obo  psi-ms_4.1.25_WithParentsAndGrandparents.txt
+..\bin\OBODataConverter.exe psi-ms_4.1.33.obo
+..\bin\OBODataConverter.exe psi-ms_4.1.33.obo  psi-ms_4.1.33_NoAncestors.txt /NoP
+..\bin\OBODataConverter.exe psi-ms_4.1.33.obo  psi-ms_4.1.33_WithParents.txt /NoG
+..\bin\OBODataConverter.exe psi-ms_4.1.33.obo  psi-ms_4.1.33_WithParentsAndGrandparents.txt
 
-..\bin\OBODataConverter.exe psi-ms_4.1.25.obo  psi-ms_4.1.25_WithDefinition_StripQuotes.txt /Def /StripQuotes /Com /NoP /NoObsolete /PK:MS1
+..\bin\OBODataConverter.exe psi-ms_4.1.33.obo  psi-ms_4.1.33_WithDefinition_StripQuotes.txt /Def /StripQuotes /Com /NoP /NoObsolete /PK:MS1
 
-..\bin\OBODataConverter.exe psi-ms_4.1.25.obo  psi-ms_4.1.25_WithDefinition_StripQuotes_IncludeObsolete.txt /Def /StripQuotes /Com /PK:MS1
+rem Use this one for DMS
+..\bin\OBODataConverter.exe psi-ms_4.1.33.obo  psi-ms_4.1.33_WithDefinition_StripQuotes_IncludeObsolete.txt /Def /StripQuotes /Com /PK:MS1
+
+rem Use this in Visual Studio
+rem ..\docs\psi-ms_4.1.33.obo psi-ms_4.1.33_WithDefinition_StripQuotes_IncludeObsolete.txt /Def /StripQuotes /Com /PK:MS1
 
 pause
