90b77416f2eafdb0491805c2efef0b2c597add3d
alchemistmatt <matthew.monroe@pnnl.gov>
2025-02-26 17:55:33 -0800

    Update batch file to use psi-ms_example.obo

--- TestData/Convert_psi-ms.bat
+++ TestData/Convert_psi-ms.bat
@@ -1,14 +1,13 @@
-..\bin\OBODataConverter.exe psi-ms_4.1.33.obo
-..\bin\OBODataConverter.exe psi-ms_4.1.33.obo  psi-ms_4.1.33_NoAncestors.txt /NoP
-..\bin\OBODataConverter.exe psi-ms_4.1.33.obo  psi-ms_4.1.33_WithParents.txt /NoG
-..\bin\OBODataConverter.exe psi-ms_4.1.33.obo  psi-ms_4.1.33_WithParentsAndGrandparents.txt
+..\bin\OBODataConverter.exe psi-ms_example.obo
+..\bin\OBODataConverter.exe psi-ms_example.obo  psi-ms_example_NoAncestors.txt /NoP
+..\bin\OBODataConverter.exe psi-ms_example.obo  psi-ms_example_WithParents.txt /NoG
+..\bin\OBODataConverter.exe psi-ms_example.obo  psi-ms_example_WithParentsAndGrandparents.txt
 
-..\bin\OBODataConverter.exe psi-ms_4.1.33.obo  psi-ms_4.1.33_WithDefinition_StripQuotes.txt /Def /StripQuotes /Com /NoP /NoObsolete /PK:MS1
+..\bin\OBODataConverter.exe psi-ms_example.obo  psi-ms_example_WithDefinition_StripQuotes.txt /Def /StripQuotes /Com /NoP /NoObsolete /PK:MS1
 
 rem Use this one for DMS
-..\bin\OBODataConverter.exe psi-ms_4.1.33.obo  psi-ms_4.1.33_WithDefinition_StripQuotes_IncludeObsolete.txt /Def /StripQuotes /Com /PK:MS1
+..\bin\OBODataConverter.exe psi-ms_example.obo  psi-ms_example_WithDefinition_StripQuotes_IncludeObsolete.txt /Def /StripQuotes /Com /PK:MS1 /Postgres
 
 rem Use this in Visual Studio
-rem ..\docs\psi-ms_4.1.33.obo psi-ms_4.1.33_WithDefinition_StripQuotes_IncludeObsolete.txt /Def /StripQuotes /Com /PK:MS1
-
+rem ..\docs\psi-ms_4.1.191.obo /O:..\docs\psi-ms_4.1.191_WithDefinition_StripQuotes_IncludeObsolete.txt /Def /StripQuotes /Com /PK:MS1 /Postgres
 pause
