76f64c34d47cc3794af187c4356ca12ec8b29dda
alchemistmatt <matthew.monroe@pnnl.gov>
2025-02-26 17:47:22 -0800

    Remove near-duplicate batch file

--- TestData/Test_Convert.bat
+++
@@ -1,9 +0,0 @@
-..\bin\OBODataConverter.exe psi-ms_3.84.0.obo
-..\bin\OBODataConverter.exe psi-ms_3.84.0.obo  psi-ms_3.84.0_NoAncestors.txt /NoP
-..\bin\OBODataConverter.exe psi-ms_3.84.0.obo  psi-ms_3.84.0_WithParents.txt /NoG
-..\bin\OBODataConverter.exe psi-ms_3.84.0.obo  psi-ms_3.84.0_WithParentsAndGrandparents.txt
-
-..\bin\OBODataConverter.exe psi-ms_3.84.0.obo  psi-ms_3.84.0_WithDefinition_StripQuotes.txt /Def /StripQuotes /Com /NoP /NoObsolete /PK:MS1
-
-..\bin\OBODataConverter.exe psi-ms_3.84.0.obo  psi-ms_3.84.0_WithDefinition_StripQuotes_IncludeObsolete.txt /Def /StripQuotes /Com /PK:MS1
-
